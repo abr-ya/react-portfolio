3d0f03cfad74aaf0aaf41d62a680cb264cf546f3
abr-ya <gamer05@list.ru>
2022-04-24 17:44:55 +0300

    feat: Add desktop navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,24 @@
+import { images } from '../../constants';
 import './navbar.scss';
 
-export default function Works() {
+const menuItems = ['home', 'about', 'work', 'skills', 'contact'];
+
+function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {menuItems.map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+import images from './images';
+
+// отключаем, т.к. могут быть ещё константы
+// eslint-disable-next-line import/prefer-default-export
+export { images };
