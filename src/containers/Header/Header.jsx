e6539689bb7ec71808a76e80d5cdd2557aef3efc
abr-ya <gamer05@list.ru>
2022-04-25 07:50:07 +0300

    feat: Update Header block

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,7 +1,58 @@
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 import './header.scss';
 
-export default function Header() {
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const circles = [images.react, images.redux, images.sass];
+
+function Header() {
   return (
-    <div>Header</div>
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-200, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.8 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">AbrYa</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">React Developer</p>
+            <p className="p-text">and ... ???</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {circles.map((circle, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="profile_bg" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
   );
 }
+
+export default Header;
